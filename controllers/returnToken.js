4bb79d8c9e8cf997f84c77dce31a3fda776245bd
Daniel Batista <62209642+danielbped@users.noreply.github.com>
2021-11-17 14:27:46 -0300

    Update returnToken.js

--- controllers/returnToken.js
+++ controllers/returnToken.js
@@ -1,5 +1,6 @@
 const HTTP_OK_STATUS = 200;
 
+//source: https://stackoverflow.com/questions/8532406/create-a-random-token-in-javascript-based-on-user-details
 const tokenGenerator = () => {
   const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.split('');
   const token = [];
